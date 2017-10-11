79c27995297c91a5b1ea8cbde994e6b2a6656faf
osama miro <o.miro650@gmail.com>
2017-10-11 01:12:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ function logWhisper(string) {
 var lowercase = "I can't hear you!"
 
 function sayHiToGrandma(lowercase) {
-  console.log("hello")
   return `${lowercase}`
 }
 
 var uppercase = "YES INDEED!"
 
 function sayHiToGrandma(uppercase){
-  console.log("HELLO")
   return `${uppercase}`
 }
