6c5dd3806c9cba56e51e5e362b09c5620980b382
osama miro <o.miro650@gmail.com>
2017-10-11 00:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-function sayHiToGrandma(logWhisper()) {
+function sayHiToGrandma(logWhisper {
   console.log("hello")
 return(`I can't hear you!`)
 }
 
-function sayHiToGrandma(logShout()){
+function sayHiToGrandma(logShout){
   console.log("HELLO")
   return(`YES INDEED!`)
 }
